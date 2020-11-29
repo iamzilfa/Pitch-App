9cab9fa5e91d19cffa933eb7cd148b0bba753dbd
Zilfa Cyamani <zilcyam@gmail.com>
2020-11-30 00:16:39 +0200

    creating files

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:123456@localhost/pitch_app'
+
+
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:123456@localhost/pitch_app_test'
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+# 'test':TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY=12345
+
+export MAIL_USERNAME=zilfeur@gmail.com
+export MAIL_PASSWORD=kigali250
+
+python3.6 manage.py server
