3a72f50cb50f48cdf4e9862eb2eeb486bd7433b2
Zilfa Cyamani <zilcyam@gmail.com>
2020-12-07 10:52:00 +0200

    last

--- app/email.py
+++ app/email.py
@@ -4,7 +4,7 @@ from . import mail
 
 
 def mail_message(subject,template,to,**kwargs):
-    sender_email = 'zilfeur@gmail.com'
+    sender_email = 'iamzilfa@gmail.com'
 
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY=12345
 
-export MAIL_USERNAME=zilfeur@gmail.com
-export MAIL_PASSWORD=kigali250
+export MAIL_USERNAME=iamzilfa@gmail.com
+export MAIL_PASSWORD=kigali123!
 
 python3.6 manage.py server
